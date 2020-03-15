e90f75ed7567454235dd26aabc41601f0404f0a6
Mohamad Belal Natafgi <bilal.natafji@gmail.com>
2020-03-16 00:43:43 +0200

    update styling, remove clip-path due to lack of edge support

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BIRDS from 'vanta/dist/vanta.birds.min';
 // import * as THREE from 'vanta/vendor/three.r95.min';
 import './hero.css';
+import Typed from 'react-typed';
 
 class Hero extends Component {
     constructor() {
@@ -19,6 +20,7 @@ class Hero extends Component {
           color1: 0xff4a,
           color2: 0x4bd4ff
         });
+
       }
       componentWillUnmount() {
         if (this.vantaEffect) this.vantaEffect.destroy();
@@ -27,9 +29,15 @@ class Hero extends Component {
         return (
             <div ref={this.vantaRef} className='hero-main'>  
                 <div className="hero-welcome">
-                        <p>Hello! I'm</p>
-                        <p className='name'>Mohamad Belal Natafgi,</p>
-                        <p>a software engineer.</p>
+                  <Typed 
+                    strings={["<p>Hello! I'm</p><p class='name'>Mohamad Belal Natafgi</p>", "<p>A Software Engineer...</p>"]} 
+                    typeSpeed={60}
+                    backSpeed={60}
+                    backDelay={3000}
+                    loop={true}
+                    showCursor={false}
+                    />
+                        
                 </div>
             </div>
         )
