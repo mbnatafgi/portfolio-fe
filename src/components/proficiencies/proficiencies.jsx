4b01089782419c562a556d6b66495b9a5bbe842a
Mohamad Belal Natafgi <bilal.natafji@gmail.com>
2020-04-08 02:16:23 +0300

    update styling

--- src/components/proficiencies/proficiencies.jsx
+++ src/components/proficiencies/proficiencies.jsx
@@ -14,13 +14,13 @@ const Proficiency = (props) => {
 const Proficiencies = (props) => {
 
     const profs = [
-        {title: 'Python', value: 0.89, max: 1},
-        {title: 'Bash', value: 0.75, max: 1},
-        {title: 'Git', value: 0.77, max: 1},
-        {title: 'MongoDB', value: 0.8, max: 1},
-        {title: 'SQL', value: 0.77, max: 1},
-        {title: 'Docker', value: 0.83, max: 1},
-        {title: 'Kubernetes', value: 0.77, max: 1},
+        {title: 'Python', value: 0.9, max: 1},
+        {title: 'Bash', value: 0.77, max: 1},
+        {title: 'Git', value: 0.79, max: 1},
+        {title: 'MongoDB', value: 0.82, max: 1},
+        {title: 'SQL', value: 0.79, max: 1},
+        {title: 'Docker', value: 0.85, max: 1},
+        {title: 'Kubernetes', value: 0.79, max: 1},
     ]
     
     return ( 
